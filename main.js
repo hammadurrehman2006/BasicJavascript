876163e1937d87e343b2d142b5673b7b4a49c43a
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2024-08-17 15:48:48 +0500

    HTML and Javascript

---
+++ main.js
@@ -0,0 +1,113 @@
+// // Qno 01
+// alert("Welcome to Our Website");
+// // Qno 02
+// alert("(Error! Please Enter a valid password");
+// // Qno 03
+// console.log("Hello! \n Messag testing");
+// // Qno 04
+// alert("Hello! This is Message-01");
+// alert("Hello! This is Message-02");
+// alert("Hello! This is Message-03");
+// // Qno 07
+// var userName = "M HammadurRehman";
+// //Qno 08
+// var myName = "M HammadurRehman";
+// console.log(`Hello my name is : ${myName}`);
+// // Qno 09
+// var message = "Hello World!";
+// alert(message);
+// // Qno 10
+// var nameUser = prompt("Enter your Name:..");
+// var age = +prompt("Enter your Age:..");
+// alert(`Hello! ${nameUser}, Your are ${age} years old`);
+// // Qno 11
+// var email = "hammadurrehman7707@gmail.com"
+// alert("My email address is " + email);
+// // Qno 13
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design);
+// console.log(design);
+// //Qno 14
+// var age = 18;
+// alert(`I am ${age} years old`);
+// //Qno 15
+// //not possible
+// //Qno 16
+// var birthYear = 2006;
+// console.log(`My birthyear is ${birthYear}`);
+// //Qno 17-----------------------------------------------------------------------------
+// //not possible
+// //Qno 18-----------------------------------------------------------------------------
+// var var1 = "variable1", var2 = "variable2", var3 = "variable3";
+//Qno 19-----------------------------------------------------------------------------
+// //5 legal variables
+// var var_1 = "Hello, world!";
+// var Var_2 = 42;
+// var vAr3 = true;
+// var $var4 = null;
+// var var145 = [28, 29, 30];
+// //5 illegal variables
+// var 1Variable = "Hello, world!";
+// var var 2 = 467;
+// var function = false;
+// var `var` = 67.8;
+// var @variable = `hello`;
+//Qno 20-----------------------------------------------------------------------------
+
+// document.write("<h1>Rules for naming JS variable </h1>");
+
+// document.write("\nVariable names can only contain Digits, Letters,Underscore and Dollar signs. For example $my_1stVariable ");
+
+// document.write("\nVariables must begin with a Digits,Letters or Dollar signs. For example$name, _name or name");
+
+// document.write("\nVariable names are case Sensitive");
+
+// document.write("\nVariable names should not be JS Reserved Words");
+// //Qno 21 and 22 ---------------------------------------------------------------------------
+// var a = +prompt("Enter 1st number : ...");
+// var b = +prompt("Enter 2nd number : ...");
+// var sum = a + b;
+// document.write("Sum of " + a + " and " + b + " is " + sum)
+// var sub = a - b;
+// document.write("\nSub of " + a + " and " + b + " is ", sub);
+// var multiply = a * b;
+// document.write("\nMultiply of " + a + " and " + b + " is ", multiply);
+// var divide = a / b;
+// document.write("Divide of " + a + " and " + b + " is ", divide);
+// //Qno 23---------------------------------------------------------------------------
+// var a;
+
+// a = 5
+// console.log(a)
+
+// a = 5;
+
+// console.log(a);
+
+// a++
+
+// console.log(a);
+
+// var b = a + 7;;
+
+// console.log(b);
+
+// b--
+
+// console.log(b);
+
+// var c = b % 3;
+
+// console.log(c)
+
+// //Qno 24---------------------------------------------------------------------------
+// var ticketPrice = 600;
+// var ticketQuantity = 5;
+// var totalCost = ticketPrice * ticketQuantity;
+// console.log(`THE COST OF BUYING ${ticketQuantity} tickets is Rs. ${totalCost}.`)
+// //Qno 25---------------------------------------------------------------------------
+// var table = +prompt("Enter any number to generate its table:...");
+// for (let i = 1; i<=10; i++){
+//     var result = table * i;
+//     document.write(table + " x " + i + " = " + result + "<br>");
+// }
